1c66c5e6fbf30e962113017d8c35c295c80c3524
Jeremias <jeremiaspion@gmail.com>
2022-10-05 13:54:10 -0300

    Criação registro

--- assets/js/User.js
+++ assets/js/User.js
@@ -1,20 +1,47 @@
     let users = [];
+    let numMinId = 1;
+    let numMaxId = 1000;
 class User {
     constructor(name, email, password){
         this.name = name;
         this.email = email;
         this.password = password;
-        this.id = this.getRandomInt(1, 1000);
+        this.id = this.verifyUsers();
+    }
+    
+    verifyUsers() {
+        if(dataBase.getUsers() == null) {
+            return this.getRandomInt();
+        }else {
+            return this.generateId();
+        }
     }
 
-    getRandomInt(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min) + min);
+    getRandomInt() {
+        let min = Math.ceil(numMinId);
+        let max = Math.floor(numMaxId);
+        let id = Math.floor(Math.random() * (max - min) + min);
+        return id;
+    }
+
+    generateId() {
+        let e_id;
+        let newId;
+        do {
+            newId = this.getRandomInt();
+            e_id = dataBase.getUsers().findIndex(e => e.id == newId);
+        } while (e_id != -1);
+        return newId;
     }
 
     verifyListUsers(newUser) {
-        dataBase.getUsers() == null ? this.addNewUser(newUser) : this.setUsers(newUser);
+        dataBase.getUsers() == null ? this.addNewUser(newUser) : this.checkUserData(newUser);
+    }
+    
+    addNewUser(newUser) {
+        users.push(newUser)
+        this.registerNewUser();
+        return true;
     }
 
     setUsers(newUser) {
@@ -28,16 +55,34 @@ class User {
         return true;
     }
 
-    addNewUser(newUser) {
-        users.push(newUser)
-        this.registerNewUser();
-        return true;
-    }
-
     registerNewUser() {
         dataBase.setUsers(users) ? this.cleanListUsers() : false;       
     }
 
+    verifyEmail(newUser) {
+        let e_email = dataBase.getUsers().findIndex(e => e.email == newUser.email);
+        return e_email;
+    }
+
+    verifyPassword(newUser) {
+        let e_password = newUser.password.length > 7;
+        return e_password;
+    }
+
+    checkUserData(newUser) {
+        if(this.verifyEmail(newUser) != -1){
+            console.log("Email já cadastrado!");
+        }else if(!this.verifyPassword(newUser)) {
+            console.log("A senha deve conter no mínimo 8 caracteres!");
+        }else {
+            this.setUsers(newUser);
+        }
+    }
+
+    /*
+     MANTER ATÉ A PÁGINA DE LOGIN
+     RETIRAR DEPOIS E REFORMULAR O CÓDIGO
+    */
     cleanListUsers() {
         console.log(users)
         users.length = 0;
--- assets/js/app.js
+++ assets/js/app.js
@@ -5,7 +5,15 @@ document.querySelector("#formRegister").addEventListener("submit", (e) => {
     let email = document.querySelector("#email").value;
     let password = document.querySelector("#password").value;
 
+    /* const ids = [1,2,3,4,5,6,7,8]; */
+
+
+    /* console.log(User.verifyEmail(email)); */
+
     let newUser = new User(name, email, password);
     newUser.verifyListUsers(newUser);
+
+    
+
     /* localStorage.removeItem("users") */
 });
